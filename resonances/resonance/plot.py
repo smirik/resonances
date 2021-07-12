053e60c7a27fd64e77e7aa662ec5980c9005e641
smirik <smirik@gmail.com>
2021-07-12 23:43:34 +0300

    WIP

--- resonances/resonance/plot.py
+++ resonances/resonance/plot.py
@@ -6,35 +6,12 @@ from pathlib import Path
 import resonances.config
 
 
-def asteroids(data, mmrs, librations, save=True, save_path='cache'):
-    if save:
-        Path(save_path).mkdir(parents=True, exist_ok=True)
-
-    for i, mmr in enumerate(mmrs):
-        asteroid(
-            data['times'],
-            data['angle'][i],
-            data['axis'][i],
-            data['ecc'][i],
-            mmr,
-            librations['status'][i],
-            librations['libration_data'][i],
-            save_path,
-        )
-        if save:
-            df_data = {
-                'times': data['times'] / (2 * np.pi),
-                'angle': data['angle'][i],
-                'a': data['axis'][i],
-                'ecc': data['ecc'][i],
-            }
-            df = pd.DataFrame(data=df_data)
-            df.to_csv('{}/data-{}-{}.csv'.format(save_path, mmr.body_name, mmr.to_s()))
-
-
 def body(sim, body: resonances.Body):
     asteroid_plot(sim.times, body.angle, body.axis, body.ecc, body.name, body.mmr, body.status, body.libration_data)
-    plt.savefig('{}/fig-{}-{}-{}.png'.format(sim.save_path, body.status, body.name, body.mmr.to_s()))
+    if sim.plot == True:
+        plt.savefig('{}/fig-{}-{}-{}.png'.format(sim.save_path, body.status, body.name, body.mmr.to_s()))
+    elif sim.plot == 'show':
+        plt.show()
 
 
 def asteroid(times, angle, axis, ecc, body_name, mmr, status, libration_data, save_path='cache'):
